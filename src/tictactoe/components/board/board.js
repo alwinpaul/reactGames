4a485edaa52e7c3b536b0031379be17bc1b5f99e
Alwin Abraham <alwin.abraham@sugarboxnetworks.com>
2019-08-05 19:00:44 +0530

    WIP : tic tac toe - winner logic creation

--- src/tictactoe/components/board/board.js
+++ src/tictactoe/components/board/board.js
@@ -7,9 +7,20 @@ class Board extends Component {
     super(props);
     this.handleSquareClick = this.handleSquareClick.bind(this);
   }
+  winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [2, 5, 8],
+    [1, 4, 7],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
   state = {
     squareValue: Array(9).fill(null),
-    isX: true
+    isX: true,
+    winningIndex: Array(3).fill(null)
   };
 
   renderRow(i) {
@@ -23,17 +34,46 @@ class Board extends Component {
     );
   }
 
+  checkForWinner(isX) {
+    let playerValue = isX ? "X" : "O";
+    let isWinner = false;
+    debugger;
+    for (let i = 0; i < this.winningCombos.length; i++) {
+        const firstIndex = this.winningCombos[i][0];
+        const secondIndex = this.winningCombos[i][1];
+        const thirdIndex = this.winningCombos[i][2];
+      if (
+        this.state.squareValue[firstIndex] == playerValue &&
+        this.state.squareValue[secondIndex] == playerValue &&
+        this.state.squareValue[thirdIndex] == playerValue
+      ) {
+          const winnerIndexs = [firstIndex, secondIndex, thirdIndex];
+          this.setState({
+              winningIndex: winnerIndexs
+          });
+          isWinner = true;
+          break;
+      }
+    };
+    if(isWinner) {
+        alert(playerValue + " Wins" )
+    }
+  }
+
   handleSquareClick(i) {
     const squares = this.state.squareValue.slice();
     if (this.state.isX) {
-        squares[i] = 'X'
+      squares[i] = "X";
     } else {
-        squares[i] = 'O'
+      squares[i] = "O";
     }
     this.setState({
-        squareValue: squares,
-        isX: !this.state.isX
-    })
+      squareValue: squares
+    });
+    this.checkForWinner(this.state.isX);
+    this.setState({
+      isX: !this.state.isX
+    });
   }
 
   render() {
